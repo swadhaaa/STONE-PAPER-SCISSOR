c3e128f36fe4df7c81c19147fe79827e68041ad1
Swadha <swadhavijay25@gmail.com>
2023-12-20 18:59:43 +0530

    Its running finally

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+extern crate rand;
 use std::io;
 use rand::Rng;
 
 fn main() {
-	
+	let mut _rng = rand::thread_rng();
 	println!("1 - Rock, 2 - Paper, 3 - Scissors");
 
 	let mut players_choice = String::new();
 	let mut players_choice_int = 0;
-
+	
 	io::stdin().read_line(&mut players_choice).expect("Cannot read the players_choice");
-
+	
+	let comp_choice = rand::thread_rng().gen_range(1..=3);
+	
+	match comp_choice {
+		1 => println!("Computer chose Rock"),
+		2 => println!("Computer chose Paper"),
+		3 => println!("Computer chose Scissors"),
+		_ => println!("Error!"),
+	}
+	
 	if players_choice == "1\n" {
 		players_choice_int = 1;
     	println!("You chose Rock");
@@ -22,15 +32,9 @@ fn main() {
 	} else {
 		println!("Error!");
 	}
-	let comp_choice = rand::thread_rng().gen_range(1, 3);
-
-	match comp_choice {
-		1 => println!("Computer chose Rock"),
-		2 => println!("Computer chose Paper"),
-		3 => println!("Computer chose Scissors"),
-		_ => println!("Error!"),
-	}
 	let compare = (players_choice_int, comp_choice);
+	
+
 	match compare {
 		(1, 1) => println!("It's a tie!"),
 		(1, 2) => println!("You lose!"),
